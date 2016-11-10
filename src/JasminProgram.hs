034cbe77f86b6a12a20fd524e4db07a81ad72d1c
Michał Piotr Stankiewicz <michal.stankiewicz@altalog.pl>
2016-11-10 01:36:30 +0100

    fix(JasminProgram.hs): mijor bug

--- src/JasminProgram.hs
+++ src/JasminProgram.hs
@@ -37,10 +37,8 @@ parseProgram s className path = let ts = myLexer s in
       liftIO $ print s
       throwError (OtherError s)
     Ok three -> do
-      liftIO $ print three
       program <- evalProgram three
       state <- get
-      liftIO $ print state
       liftIO $ writeFile path $ unlines ((header className) ++ (mainHeader (biggestStack state) (nextLocal state)) ++ program ++ mainFooter)
   
 evalProgram :: Program -> MyRunner [String]
@@ -68,23 +66,36 @@ evalStmt (SAss ident e) = do
 evalStmt (SExp e) = do
   state <- get
   (instructions, stackSize) <- evalExp e
+  liftIO $ print $ show stackSize
   put $ MyState (env state) (nextLocal state) (max (stackSize + 1) (biggestStack state)) -- TODO(mps)
   return $ printInt instructions
   
 evalExp :: Exp -> MyRunner ([String], Integer)
 evalExp (ExpLit i) = return $ (intToStack i, 1)
-evalExp (ExpAdd lhs rhs) = evalArith lhs rhs Add 
-evalExp (ExpSub lhs rhs) = evalArith lhs rhs Sub 
-evalExp (ExpMul lhs rhs) = evalArith lhs rhs Mul  
-evalExp (ExpDiv lhs rhs) = evalArith lhs rhs Div  
+evalExp (ExpAdd lhs rhs) = evalArithWitoutOrder lhs rhs Add 
+evalExp (ExpSub lhs rhs) = evalArithWithOrder lhs rhs Sub 
+evalExp (ExpMul lhs rhs) = evalArithWitoutOrder lhs rhs Mul  
+evalExp (ExpDiv lhs rhs) = evalArithWithOrder lhs rhs Div  
 evalExp (ExpVar ident) = do
   state <- get
   return (loadLocal $ ((env state) Map.! ident), 1)
+  
+stackSum :: Integer -> Integer -> Integer
+stackSum a b
+  | a == b = a + 1
+  | a < b = b
+  | otherwise = a
 
-evalArith :: Exp -> Exp -> Operation -> MyRunner ([String], Integer)
-evalArith lhs rhs op = do
+evalArithWitoutOrder :: Exp -> Exp -> Operation -> MyRunner ([String], Integer)
+evalArithWitoutOrder lhs rhs op = do
   (lhs_inst, lhs_stack) <- evalExp lhs
   (rhs_inst, rhs_stack) <- evalExp rhs
   if lhs_stack > rhs_stack
-    then return (operation lhs_inst rhs_inst op, rhs_stack + 1) -- TODO - CALCULATE STACK
-    else return (operation rhs_inst lhs_inst op, lhs_stack + 1) -- TODO - CALCULATE STACK
+    then return (operation lhs_inst rhs_inst op, stackSum lhs_stack rhs_stack) -- TODO - CALCULATE STACK
+    else return (operation rhs_inst lhs_inst op, stackSum lhs_stack rhs_stack) -- TODO - CALCULATE STACK
+
+evalArithWithOrder :: Exp -> Exp -> Operation -> MyRunner ([String], Integer)
+evalArithWithOrder lhs rhs op = do
+  (lhs_inst, lhs_stack) <- evalExp lhs
+  (rhs_inst, rhs_stack) <- evalExp rhs
+  return (operation lhs_inst rhs_inst op, stackSum lhs_stack rhs_stack) -- TODO - CALCULATE STACK
